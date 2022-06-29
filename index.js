882046ce222784f86a7b04c4ea73ef89cf464d52
Rafael <aguilera.morales.rafa@gmail.com>
2022-06-29 12:58:34 -0700

    working

---
+++ index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var { google } = require('googleapis');
+var app = express(); 
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true})); 
+
+app.get("/", (req,res) => {
+    res.render("contact");
+});
+
+app.post('/', async (req, res) => {
+
+    const {firstName, lastName, email} = req.body;
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+});
+    
+
+const client = await auth.getClient();
+
+const googleSheets = google.sheets({version: "v4", auth:client});
+
+const spreadsheetID = "1dDqVCN26jUwdsMx1m7DWduyVkAQ1hNPdnt7CkBJ39JQ";
+
+const metaData = await googleSheets.spreadsheets.get({
+
+    auth: auth,
+    spreadsheetId: spreadsheetID,
+});
+
+const getRows = await googleSheets.spreadsheets.values.get({
+    auth: auth,
+    spreadsheetId: spreadsheetID,
+    range: "Sheet1",
+});
+
+await googleSheets.spreadsheets.values.append({
+   
+    auth: auth,
+    spreadsheetId: spreadsheetID,
+    range: "Sheet1!A:C",
+    valueInputOption: "USER_ENTERED",
+    resource: {
+        values: [
+            [firstName, lastName, email]
+        ],
+    }
+
+});
+
+res.send(`Thank you ${firstName} ${lastName} ${email}`)
+});
+
+app.listen(8080, () => {
+    console.log('listening on http://localhost:8080')});
--- server.js
+++ server.js
@@ -1,32 +1,33 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const morgan = require('morgan');
+// const bodyParser = require('body-parser');
 
-const app = express();
+// const app = express();
 
-app.use(morgan('dev'))
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(morgan('dev'))
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('views','./views');
+// app.set('views','./views');
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index')});
-    
+// app.get('/', (req, res) => {
+//     res.render('index')});
 
-app.get('/contact', (req, res) => {
-    res.render('contact');
-    });
+
+// app.get('/contact', (req, res) => {
+//     res.render('contact');
+//     });
         
-app.post('/thanks', (req, res) => {
-    res.render('thanks', { contact: req.body })
-    });
+// app.post('/thanks', (req, res) => {
+//     res.render('thanks', { contact: req.body })
+//     });
     
-app.listen(8080, () => {
-        console.log('listening on http://localhoast:8080')});
+// app.listen(8080, () => {
+//         console.log('listening on http://localhoast:8080')});
 
+//**********************
 
 // var profile = require('./profile')
 
